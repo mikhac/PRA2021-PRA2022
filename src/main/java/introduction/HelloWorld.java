cbf3567c9a50981b860d3eed214c7fa0100356cf
mikhac <mikhac@st.amu.edu.pl>
2021-10-23 17:35:58 +0200

    Add logger and build system

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
-    public static void main(String [ ] args) {
+    static Logger logger = Logger.getLogger("name");
+
+    public static void main(String[] args) {
         System.out.println("Hello! Witaj!");
+        logger.info("hello from logger");
     }
 }
